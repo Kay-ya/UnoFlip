c7a74ce1661b53946da6b72fd4f626d9b7f0ad30
nikitasv457 <147352295+nikitasv457@users.noreply.github.com>
2023-10-18 20:24:21 -0400

    Updated Deck.java

--- src/Deck.java
+++ src/Deck.java
@@ -1,112 +1,73 @@
-import java.security.Key;
 import java.util.*;
 
 public class Deck {
-    private int inDeckCards;
-    String text;
-    private List<Card> deckLight;
-    private List<Card> deckDark;
 
-    private String card;
+    private List<Card> cards;
 
-    HashMap<Card, Card> cardDictionary;
+    private List<Card> discardPile;
 
+    private List<Card> drawPile; //Nikita
 
-    public Deck() {
-        //card = new Card[224];
-        deckLight = new ArrayList<>();
-        deckDark = new ArrayList<>();
-        cardDictionary = new HashMap<Card, Card>();
+    public Deck(){
+        this.cards = new ArrayList<>();
+        this.drawPile = new ArrayList<>(); //Nikita
+        List<CardSideDetails> lightSideDetails = createSideDetails(new Color[]{Color.BLUE, Color.GREEN, Color.RED, Color.YELLOW});
+        List<CardSideDetails> darkSideDetails = createSideDetails(new Color[]{Color.PINK, Color.TEAL, Color.ORANGE, Color.PURPLE});
+
+        for (int i = 0; i < lightSideDetails.size() && i < darkSideDetails.size(); i++) {
+            CardSideDetails light = lightSideDetails.get(i);
+            CardSideDetails dark = darkSideDetails.get(i);
+            cards.add(new Card(light.type, light.color, dark.type, dark.color));
+        }
     }
 
-    private void populateDeck() { //creating the cards light side and dark side
-        //light side
-        //populates numbers
-        for(int i=0; i<4; i++) {
-            for (int j = 0; j < 9; j++) {
-                for (int k = 0; k < 2; k++) {
-                    Card card = new Card(Card.Colour.getColour(i), Card.Value.getValue(j));
-                    deckLight.add(card);
+    private static class CardSideDetails {
+        CardType type;
+        Color color;
+        CardSideDetails(CardType type, Color color) {
+            this.type = type;
+            this.color = color;
+
+        }
+    }
+    private List<CardSideDetails> createSideDetails(Color[] colors) {
+        List<CardSideDetails> sideDetails = new ArrayList<>();
+        // might be a cleaner way of initializing one side
+        for (Color color: colors){
+            for (int i = 0; i < 2; i++){
+                for (CardType type: CardType.values()){
+                    sideDetails.add(new CardSideDetails(type, color));
                 }
             }
         }
-        //populates special cards except wild cards
-        for(int i=0; i<4; i++) {
-            for (int j=9; j<13; j++){
-                for(int k=0;k<2;k++){
-                    Card card = new Card(Card.Colour.getColour(i),Card.Value.getValue(j));
-                    deckLight.add(card);
-                }
-            }
-        }
-        //populates wild cards
-        for(int j=15; j<17; j++){
-            for(int k=0; k<4; k++){
-                Card card = new Card(Card.Colour.WILD, Card.Value.getValue(j));
-                deckLight.add(card);
-            }
-        }
-
-        //dark side
-        //populates numbers
-        for(int i=4; i<8; i++){
-            for(int j=0; j< 9; j++){
-                for(int k=0; k<2;k++){
-                    Card card = new Card(Card.Colour.getColour(i), Card.Value.getValue(j));
-                    deckDark.add(card);
-                }
-            }
-        }
-        //populates special cards except wild cards
-        for(int i=4; i<8; i++) {
-            for (int j=11; j<15; j++){
-                for(int k=0;k<2;k++){
-                    Card card = new Card(Card.Colour.getColour(i),Card.Value.getValue(j));
-                    deckDark.add(card);
-                }
-            }
-        }
-        //populates wild cards
-        for(int j=16; j<18; j++){
-            for(int k=0; k<4; k++){
-                Card card = new Card(Card.Colour.WILD, Card.Value.getValue(j));
-                deckDark.add(card);
-            }
-        }
+        Collections.shuffle(sideDetails);
+        return sideDetails;
     }
 
-    public void shuffleDeck(){ //shuffling both the light deck and dark deck beforre adding it to the hashmap/dictinoary
-        Collections.shuffle(deckDark);
-        Collections.shuffle(deckLight);
-    }
-    public void createFlipUnoCard(){ //generating both sides dark and light of the card
-        this.shuffleDeck(); //shuffle deck function from above
-
-        for(int i=0; i<112; i++) {
-            cardDictionary.put(deckLight.get(i), deckDark.get(i));
-        }
-        //System.out.println(cardDictionary);
-        //cardDictionary.elements();
+    public void shuffle(){
+        Collections.shuffle(cards);
     }
 
-    public Card returnCard(){ //returning null should return a card for hand
-        Card card = new Card(Card.Colour.GREEN, Card.Value.FIVE);
-        //Card c = cardDictionary.get("BLUE_SIX");                    // the key should be a Card object
-        Card c = cardDictionary.get(card);
-        return c;
+    public Card drawCard(){
+        Card card = this.cards.get(0);
+        cards.remove(card);
+        return card;
     }
 
-    public HashMap<Card,Card> returnDeck(){
-        this.populateDeck();
-        this.createFlipUnoCard();
-        return cardDictionary;
+    // maybe game class handles discard pile instead
+    public void emptyDiscard(){
+        this.cards.addAll(discardPile);
+        this.discardPile = new ArrayList<>();
     }
 
-    public static void main(String[] args) {
-        Deck d = new Deck();
-        d.populateDeck();
-        System.out.println(d.returnDeck());
-        System.out.println(d.returnCard());
-        System.out.println(d.cardDictionary.size());
+    //Nikita
+    public void addToDiscardPile(Card card){
+        discardPile.add(card);
     }
+
+    //Nikita
+    public void removeFromDrawPile(Card card){
+        drawPile.remove(card);
+    }
+
 }
