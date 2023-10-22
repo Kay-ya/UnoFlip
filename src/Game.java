b94f3c55fa62db98d11ddea00d64110ccf72e79c
Samson Ha <whiteram81@gmail.com>
2023-10-22 14:50:58 -0400

    nextPlayerIndex private -> public

--- src/Game.java
+++ src/Game.java
@@ -38,7 +38,7 @@ public class Game {
      * @param direction
      * @return int
      */
-    private static int nextPlayerIndex(int curIndex, int numPlayers, Boolean direction){
+    public static int nextPlayerIndex(int curIndex, int numPlayers, Boolean direction){
         if (direction) {
             curIndex -= 1;
             if (curIndex == -1){
