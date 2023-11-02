b4aedf4daa1b038c390dd119b450dc9dc2b45551
Samson Ha <whiteram81@gmail.com>
2023-11-02 12:24:41 -0400

    Add Draw Deck button and Card button actions

--- src/GameController.java
+++ src/GameController.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Objects;
 
 public class GameController implements ActionListener {
     Game game;
@@ -9,27 +10,47 @@ public class GameController implements ActionListener {
     public GameController(Game game, GameView view){
         this.game = game;
         this.view = view;
-        for (JButton cardButton : view.getCardButtons()) {
-            cardButton.addActionListener(this);
-        }
+        addCardListeners();
         view.getDeckButton().addActionListener(this);
     }
 
+    public void addCardListeners(){
+        for (JButton cardButton : view.getCardButtons()) {
+            cardButton.addActionListener(this);
+        }
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
+        System.out.println("Action performed");
         JButton source = (JButton) e.getSource();
-
-        for ( int i = 0; i < view.getCardButtons().length; i++) {
-            JButton button = view.getCardButtons()[i];
-            if (source == button) {
-
+        Card handCard;
+        Card topDiscardCard = game.getDeck().topCardFromDiscardPile();
+        int i = 0;
+        view.updateView(game);
+        for (JButton button : view.getCardButtons()){
+            handCard = game.getCurrentPlayer().getHand().get(i);
+            if (Objects.equals(source.getText(), button.getText())) {
+                if (game.getSide() && (handCard.getBrightColor() == topDiscardCard.getBrightColor() || handCard.getBrightCardType() == topDiscardCard.getBrightCardType())){
+                    game.getCurrentPlayer().removeCardFromHand(handCard);
+                    game.getDeck().addToDiscardPile(handCard);
+                }
+                else if (!game.getSide() && (handCard.getDarkColor() == topDiscardCard.getDarkColor() || handCard.getDarkCardType() == topDiscardCard.getDarkCardType())){
+                    game.getCurrentPlayer().removeCardFromHand(handCard);
+                    game.getDeck().addToDiscardPile(handCard);
+                }
                 System.out.println("Card button clicked: " + button.getText());
-                return;
+                break;
             }
+            i++;
         }
 
         if (source == view.getDeckButton()) {
+            Card drawnCard = game.getDeck().drawCard();
+            game.getCurrentPlayer().addCardToHand(drawnCard);
             System.out.println("Deck button clicked");
         }
+
+        view.updateView(game);
+        addCardListeners();
     }
 }
--- src/GameView.java
+++ src/GameView.java
@@ -10,6 +10,9 @@ public class GameView extends JFrame{
     public GameView(){
         this.setTitle("Uno Flip");
         game = new Game();
+        cardsPanel = new JPanel();
+        centerPanel = new JPanel();
+        deckButton = new JButton("draw from deck");
         for (int i = 0; i < 4; i++){
             Player p = new Player( "Player " + i );
             for (int j = 0; j < 7; j++) {
@@ -18,10 +21,8 @@ public class GameView extends JFrame{
             game.addPlayer(p);
         }
         game.setCurrentPlayer(game.getPlayers().get(0));
-
-        createCardPanel(game);
-        createCenterPanel(game);
-
+        updateView(game);
+        controller = new GameController(game, GameView.this);
         // frame setup
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout());
@@ -29,32 +30,36 @@ public class GameView extends JFrame{
         this.add(centerPanel, BorderLayout.CENTER);
         this.pack();
         this.setVisible(true);
-    }
-
-    private void createCardPanel(Game game){
-        cardsPanel = new JPanel();
-        cardButtons = new JButton[game.getCurrentPlayer().getHand().size()];
-        for (int i = 0; i < game.getCurrentPlayer().getHand().size(); i++) {
-            String cardName = game.getCurrentPlayer().getHand().get(i).toString(game.getSide());
-            cardButtons[i] = new JButton(cardName);
-            cardsPanel.add(cardButtons[i]);
-        }
 
     }
 
-    private void createCenterPanel(Game game){
-        centerPanel = new JPanel();
-        deckButton = new JButton("draw from deck");
-        discardButton = new JButton(game.getDeck().topCardFromDiscardPile().toString(game.getSide()));
-        centerPanel.add(deckButton);
-        centerPanel.add(discardButton);
-    }
     public JButton[] getCardButtons() {
         return cardButtons;
     }
 
     public JButton getDeckButton() {
         return deckButton;
+    }
+
+    public void updateView(Game game){
+        // bottom cards panel
+        cardsPanel.removeAll();
+        cardButtons = new JButton[game.getCurrentPlayer().getHand().size()];
+        for (int i = 0; i < game.getCurrentPlayer().getHand().size(); i++) {
+            String cardName = game.getCurrentPlayer().getHand().get(i).toString(game.getSide());
+            cardButtons[i] = new JButton(cardName);
+            cardsPanel.add(cardButtons[i]);
+        }
+        cardsPanel.revalidate();
+        cardsPanel.repaint();
+
+        // center panel
+        centerPanel.removeAll();
+        discardButton = new JButton(game.getDeck().topCardFromDiscardPile().toString(game.getSide()));
+        centerPanel.add(deckButton);
+        centerPanel.add(discardButton);
+        centerPanel.revalidate();
+        centerPanel.repaint();
     }
 
     public static void main(String[] args) {
