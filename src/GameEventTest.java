769188da2579c8594cc2cac24bedb1ed499dcee5
Samson Ha <whiteram81@gmail.com>
2023-11-11 17:26:50 -0500

    Change imports

--- src/GameEventTest.java
+++ src/GameEventTest.java
@@ -1,13 +1,12 @@
-import static org.junit.Assert.*;
-import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-
-public class GameEventTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+class GameEventTest {
     GameEvent eventTrue, eventFalse;
     Card drawCard, removeHandCard;
     @BeforeEach
-    void setUp() {
+    void setsUp() {
         drawCard = new Card(CardType.DRAW, Color.BLUE, CardType.FIVE, Color.PINK);
         removeHandCard = new Card(CardType.DRAW, Color.GREEN, CardType.WILD, Color.TEAL);
         eventTrue = new GameEvent(new Game(), drawCard, removeHandCard, true);
