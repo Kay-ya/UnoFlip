fd49f7a433e1836683370f1f49475ea204b626f3
Kay-ya <62533585+Kay-ya@users.noreply.github.com>
2023-11-20 22:04:05 -0500

    Update GameEvent.java
    GameEvent.java updated

--- src/GameEvent.java
+++ src/GameEvent.java
@@ -10,10 +10,17 @@ public class GameEvent extends EventObject {
     Card drawCard;
     Card removeHandCard;
     Boolean status;
-
-
+    Boolean cardPlaced;
     Player player;
-    //GameController gameController;
+
+    /**
+     * Creates a GameEvent object.
+     *
+     * @param game           The game instance.
+     * @param drawCard       The card drawn during the game event.
+     * @param removeHandCard The card removed from the player's hand during the game event.
+     * @param status         The status of the game event.
+     */
     public GameEvent(Game game, Card drawCard, Card removeHandCard, Boolean status) {
         super(game);
         this.drawCard = drawCard;
@@ -26,19 +33,47 @@ public class GameEvent extends EventObject {
         this.player = player;
     }
 
+    public GameEvent(Game game, Boolean cardPlaced){
+        super(game);
+        this.cardPlaced = cardPlaced;
+    }
+
+    /**
+     * Gets a string representation of the card drawn during the game event.
+     *
+     * @return A string representation of the drawn card.
+     */
     public String getDrawCard(){
         return drawCard.toString();
     }
 
+    /**
+     * Gets a string representation of the card removed from the player's hand during the game event.
+     *
+     * @return A string representation of the removed card.
+     */
     public String getRemovedCard(){
         return removeHandCard.toString();
     }
 
+    /**
+     * Gets the status of the game event.
+     * @return The status of the game event, indicating a specific condition or action.
+     */
     public boolean getStatus(){
         return this.status;
     }
 
+    /**
+     * Returns the player.
+     * @return Player
+     */
     public Player getPlayer() {
         return player;
     }
+
+    public Boolean getCardPlaced() {
+        return cardPlaced;
+    }
+
 }
