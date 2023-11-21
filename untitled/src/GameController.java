575c54d518f9fd270b0b14218f7434e82c11f329
Nikita <147352295+nikitasv457@users.noreply.github.com>
2023-11-20 21:57:00 -0500

    Implemented AI player

--- untitled/src/GameController.java
+++ untitled/src/GameController.java
@@ -3,6 +3,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Objects;
 
+/**
+ * This class serves as the controller in the MVC pattern for the Uno Flip card game.
+ * It handles actions such as clicking on cards or the deck, and makes updates to the game state based on this.
+ */
 public class GameController implements ActionListener {
     Game game;
     GameView view;
@@ -11,7 +15,14 @@ public class GameController implements ActionListener {
     String chosenWildCardColor;
     int size;
     Card handCard, topDiscardCard, drawnCard;
+    Player player = new Player("");
 
+    /**
+     * Constructor for the GameController class.
+     *
+     * @param game The Game object representing the UnoFlip! game.
+     * @param view The GameView object representing the graphical user interface of the game.
+     */
     public GameController(Game game, GameView view){
         this.game = game;
         this.view = view;
@@ -21,20 +32,34 @@ public class GameController implements ActionListener {
         chosenWildCardColor = "";
     }
 
+    /**
+     * Adds action listeners to all card buttons in the player's hand.
+     */
     public void addCardListeners(){
         for (JButton cardButton : view.getCardButtons()) {
             cardButton.addActionListener(this);
         }
     }
 
+    /**
+     * Returns the wild light card.
+     * @return String
+     */
     public String wildLightCard()
     {
         chosenWildCardColor = view.getWildLightCardColor();
         return chosenWildCardColor;
     }
+
+    /**
+     * Handles the action events triggered by user interactions.
+     *
+     * @param e The ActionEvent representing the user's action.
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         System.out.println("Action performed");
+        boolean isMatchFound = false;
         source = (JButton) e.getSource();
         topDiscardCard = game.getDeck().topCardFromDiscardPile();
         int i = 0;
@@ -44,6 +69,7 @@ public class GameController implements ActionListener {
         }
         for (JButton button : view.getCardButtons()) {
             handCard = game.getCurrentPlayer().getHand().get(i);
+            System.out.println("$$$$ HAND CARD " + handCard.getBrightColor() + " " + handCard.getBrightCardType() );
             if ((source.getText().equals("WILD WILD") || (source.getText().equals("WILD_DRAW WILD")))) {
                 if (game.getSide() && handCard.getBrightCardType() == CardType.WILD) {
                     game.setChosenWildLightCardColor(view.getWildLightCardColor());
@@ -55,25 +81,41 @@ public class GameController implements ActionListener {
                     game.setChosenWildDarkCardColor(view.getWildDarkCardColor());
                 }
             }
-            if (Objects.equals(source.getText(), button.getText())) {
-                game.placeCards(handCard, topDiscardCard);
-                System.out.println("Card button clicked: " + button.getText());
+            System.out.println("source.getText() " + source.getText() );
+            System.out.println("button.getText() " + button.getText() );
+//            if (Objects.equals(source.getText(), button.getText())) {
+//                game.placeCards(handCard, topDiscardCard);
+//                System.out.println("Card button clicked: " + button.getText());
+//                break;
+//            }
+
+            //----------------- Human player replaced with AI player
+            if(e.getActionCommand() == "Next Player"){
+                game.getCurrentPlayer().displayHand(true);
+                if(handCard.getBrightColor() == topDiscardCard.getBrightColor() || handCard.getBrightCardType() == topDiscardCard.getBrightCardType()) {
+                    game.placeCards(handCard, topDiscardCard);
+                    isMatchFound = true;
+                }
+                game.getCurrentPlayer().displayHand(true);
+            }
+            if(isMatchFound){
+                game.returnNextPlayer();
                 break;
             }
+            //-----------------
             i++;
+        }
+        if(!isMatchFound){
+            drawnCard = game.getDeck().drawCard();
+            game.getCurrentPlayer().addCardToHand(drawnCard);
+            System.out.println("New Card Drawn");
         }
         if (source == view.getDeckButton()) {
             drawnCard = game.getDeck().drawCard();
             game.getCurrentPlayer().addCardToHand(drawnCard);
             System.out.println("Deck button clicked");
         }
-        if(e.getActionCommand() == "Next Player"){
-            //game.nextPlayerIndex(z, view.n, game.getDirection());
-            //game.getCurrentPlayer().getHand();
-            game.returnNextPlayer();
-        }
         view.updateView(game);
         addCardListeners();
-        //new GameEvent(game, this, removeCard);
     }
 }
