fa2469efcbd31a3ba26b41beee3908c37b7a208d
Nikita <147352295+nikitasv457@users.noreply.github.com>
2023-11-25 18:20:32 -0500

    Added bug fixes, implemented AI player

--- untitled/src/Game.java
+++ untitled/src/Game.java
@@ -1,35 +1,33 @@
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
-    private ArrayList<Player> players; //Stores the player name in an ArrayList
+    private ArrayList <Player> players; //Stores the player name in an ArrayList
     private Player currentPlayer;
-    List<GameUpdate> updateView;
-    GameView view;
+    private Player displayedPlayer;
     private Boolean direction; // true (default) = clockwise,
     private Deck deck;
-    boolean status;
     private Boolean side; // true = Bright side, false = Dark side
+    private List<GameView> views;
 
-    private static String chosenWildLightCardColor = "";
+    private Status status;
 
-    private static String chosenWildDarkCardColor = "";
-    public Game() {
+    public Game(){
         this.players = new ArrayList<>();
         this.direction = false;
         this.currentPlayer = null;
+        this.displayedPlayer = null;
         this.deck = new Deck();
         this.side = true;
-        this.status = false;
-        updateView = new ArrayList<>();
-        //view = new GameView();
+        this.views = new ArrayList<>();
     }
 
     public ArrayList<Player> getPlayers() {
         return players;
     }
 
-    public void addPlayer(Player player) {
+    public void addPlayer( Player player) {
         players.add(player);
     }
 
@@ -65,133 +63,194 @@ public class Game {
         side = !side;
     }
 
+    public Status getStatus(){
+        return this.status;
+    }
+    public void addView(GameView view){
+        this.views.add(view);
+    }
+
+    public void initialize(int numPlayers, int numAIPlayers){ // need test, possible point of failure
+        if (numPlayers < 1) {
+            throw new IllegalArgumentException("Number of players must be at least 1");
+        }
+
+        Player p;
+        for (int i = 0; i < numPlayers; i++){
+            p = new Player( "Human Player " + (i+1) );
+            for (int j = 0; j < 7; j++) {
+                p.addCardToHand(getDeck().drawCard());
+            }
+            addPlayer(p);
+        }
+        for (int i = 0; i < numAIPlayers; i++){
+            p = new Player( "AI Player " + (i+1) );
+            for (int j = 0; j < 7; j++) {
+                p.addCardToHand(getDeck().drawCard());
+            }
+            addPlayer(p);
+        }
+        currentPlayer = players.get(0);
+        displayedPlayer = currentPlayer;
+    }
     /**
      * Returns the player index of the card to see which direction the game is being played.
-     *
-     * @param curIndex   current index
+     * @param curIndex current index
      * @param numPlayers number of players in game
-     * @param direction  clockwise or counter-clockwise
+     * @param direction clockwise or counter-clockwise
      * @return int
      */
-    public int nextPlayerIndex(int curIndex, int numPlayers, Boolean direction) {
+    public int nextPlayerIndex(int curIndex, int numPlayers, Boolean direction){
         if (direction) {
             curIndex -= 1;
-            if (curIndex == -1) {
-                curIndex = numPlayers - 1;
+            if (curIndex == -1){
+                curIndex = numPlayers-1;
             }
-        } else {
+        }
+        else{
             curIndex = (curIndex + 1) % numPlayers;
         }
         return curIndex;
     }
 
-    public void returnNextPlayer() {
-        int i = nextPlayerIndex(getPlayers().indexOf(currentPlayer), 4, getDirection());
+    public void nextPlayer(CardColor selectedColor) {
+        ArrayList<Card> cards = new ArrayList<>();
+        boolean isMatchFound = false;
+        Card topDiscardCard = deck.topCardFromDiscardPile();
+
+        //-----------------FOR AI ----------------------------------------------------
+        if(getCurrentPlayer().getName().contains("AI")){
+
+
+           // placeCard(cardIndex, selectedColor);
+            cards = displayedPlayer.getHand();
+            int i=0;
+            for(Card handCard : cards){ //Loops over hand cards of current player
+                System.out.println(handCard.getBrightCardType() + " : " + handCard.getBrightCardType() + " : " + getSide());
+                if(getSide() && (handCard.getBrightColor() == topDiscardCard.getBrightColor() || handCard.getBrightCardType() == topDiscardCard.getBrightCardType() || (handCard.getBrightColor() == CardColor.WILD))) {
+                    selectedColor = CardColor.RED; //Sets the default colour of AI Player WILD CARD to RED
+                    placeCard(i, selectedColor);
+                    isMatchFound = true;
+                    break;
+                }
+                if(!getSide() && (handCard.getDarkColor() == topDiscardCard.getDarkColor() || handCard.getDarkCardType() == topDiscardCard.getDarkCardType() || (handCard.getDarkColor() == CardColor.WILD))) {
+
+                    selectedColor = CardColor.PINK; //Sets the default colour of AI Player WILD CARD to PINK
+                    placeCard(i, selectedColor);
+                    isMatchFound = true;
+                    break;
+                }
+                i++;
+            }
+            if(!isMatchFound){
+                getCurrentPlayer().addCardToHand(getDeck().drawCard());
+                System.out.println("New Card Drawn");
+            }
+        }
+        //--------------------------------------------------------------------------
+
+        int i = nextPlayerIndex(players.indexOf(currentPlayer), players.size(), direction);
         setCurrentPlayer(players.get(i));
-        System.out.println(i);
-        for (GameUpdate view : updateView) {
-            view.handlePlayerUnoUpdate(new GameEvent(this, players.get(i)));
+        System.out.println("getCurrentPlayer 2: " + getCurrentPlayer().getName());
+        displayedPlayer = currentPlayer;
+        //updateStatus();
+        for (GameUpdate view : views) {
+            view.handleNextPlayerEvent(new NextPlayerEvent(this, currentPlayer));
         }
     }
-    public void addGameView(GameUpdate view) {
-        updateView.add(view);
+
+    public void drawCard() {
+        Card drawnCard = getDeck().drawCard();
+        displayedPlayer.addCardToHand(drawnCard);
+        //updateStatus();
+        for (GameUpdate view : views) {
+            view.handleDrawCardEvent(new DrawCardEvent(this, displayedPlayer.getHand()));
+        }
     }
+    public boolean placeCard(int cardIndex, CardColor selectedColor) {
+        Card handCard = displayedPlayer.getHand().get(cardIndex);
+        System.out.println("HAND card  " + handCard.getBrightCardType());
+        System.out.println("In placeCard handCard" + handCard.getBrightCardType() + " : " + handCard.getDarkCardType());
+        Card topDiscardCard = deck.topCardFromDiscardPile();
+        int index = nextPlayerIndex(players.indexOf(displayedPlayer), players.size(), direction);
+        Player nextPlayer = players.get(index);
+        boolean isPlaced = false;      //Returns status of placeCards - Milestone  3-----------------
 
-    public void replaceDeckCard(Card handCard) {
-        this.getCurrentPlayer().removeCardFromHand(handCard);
-        this.getDeck().addToDiscardPile(handCard);
-
-    }
-
-    public void setBrightWildCardColor(Card handCard, Card topDiscardCard, String chosenWildCardColor){
-        topDiscardCard.setBrightColor(Color.valueOf(chosenWildCardColor));
-        handCard.setBrightColor(Color.valueOf(chosenWildCardColor));
-    }
-    public void setDarkWildCardColor(Card handCard, Card topDiscardCard, String chosenWildCardColor){
-        topDiscardCard.setDarkColor(Color.valueOf(chosenWildCardColor));
-        handCard.setDarkColor(Color.valueOf(chosenWildCardColor));
-    }
-
-
-    public void placeCards(Card handCard, Card topDiscardCard) {
-        if (this.getSide() && (handCard.getBrightColor() == topDiscardCard.getBrightColor() || handCard.getBrightCardType() == topDiscardCard.getBrightCardType())) {
-            replaceDeckCard(handCard);
+        // if bright and color or card type match
+        if (side && (handCard.getBrightColor() == topDiscardCard.getBrightColor() || handCard.getBrightCardType() == topDiscardCard.getBrightCardType())) {
             if (handCard.getBrightCardType() == CardType.FLIP) {
                 flipSide();
-                status = false;
-            } else if (handCard.getBrightCardType() == CardType.DRAW) {
-                returnNextPlayer();
-                this.getCurrentPlayer().addCardToHand(deck.drawCard());
-
-                status = false;
-            } else if (handCard.getBrightCardType() == CardType.REVERSE) {
-                this.getCurrentPlayer().addCardToHand(deck.drawCard());
-                flipDirection();
-                for (int i = 0; i < players.size(); i++) {
-                    System.out.println(players.get(i).getName());
-                }
-                status = false;
             }
+            else if (handCard.getBrightCardType() == CardType.DRAW) {
+                nextPlayer.addCardToHand(deck.drawCard());
+            }
+            else if (handCard.getBrightCardType() == CardType.REVERSE) {
+                flipDirection();
+            }
+            else if (handCard.getBrightCardType() == CardType.SKIP) {
+                int i = nextPlayerIndex(players.indexOf(currentPlayer), players.size(), direction);
+                setCurrentPlayer(players.get(i));
+            }
+            displayedPlayer.removeCardFromHand(handCard);
+            deck.addToDiscardPile(handCard);
+            isPlaced = true;  //Returns status of placeCards - Milestone  3-----------------
+
         }
+        // if dark and color or card type match
         else if (!this.getSide() && (handCard.getDarkColor() == topDiscardCard.getDarkColor() || handCard.getDarkCardType() == topDiscardCard.getDarkCardType())) {
-            this.getCurrentPlayer().removeCardFromHand(handCard);
-            this.getDeck().addToDiscardPile(handCard);
-            status = false;
-            //removeCard = handCard;
-            replaceDeckCard(handCard);
             if (handCard.getDarkCardType() == CardType.FLIP) {
                 flipSide();
-                //status = false;
             } else if (handCard.getDarkCardType() == CardType.DRAW) {
-                returnNextPlayer();
-                for (int i = 0; i < 5; i++) {
-                    this.getCurrentPlayer().addCardToHand(deck.drawCard());
+                for (int j = 0; j < 5; j++) {
+                    nextPlayer.addCardToHand(deck.drawCard());
                 }
-                //status = false;
             }
             else if (handCard.getDarkCardType() == CardType.REVERSE) {
-                this.getCurrentPlayer().addCardToHand(deck.drawCard());
                 flipDirection();
             }
-            status = false;
-        }
-        else if (this.getSide() && handCard.getBrightCardType() == CardType.WILD){
-        setBrightWildCardColor(handCard, topDiscardCard, chosenWildLightCardColor);
-        replaceDeckCard(handCard);
+            else if (handCard.getBrightCardType() == CardType.SKIP) {
+                int i = nextPlayerIndex(players.indexOf(currentPlayer), players.size(), !direction);
+                setCurrentPlayer(players.get(i));
+            }
+            displayedPlayer.removeCardFromHand(handCard);
+            deck.addToDiscardPile(handCard);
+            isPlaced = true;   //Returns status of placeCards - Milestone  3----------------
 
         }
-         else if (this.getSide() && handCard.getBrightCardType() == CardType.WILD_DRAW){
-         replaceDeckCard(handCard);
-         setBrightWildCardColor(handCard, topDiscardCard, chosenWildLightCardColor);
-         replaceDeckCard(handCard);
-         for(int i =0; i<2; i++) {
-            this.getCurrentPlayer().addCardToHand(deck.drawCard());
-         }
-         status = false;
-         } else if (!this.getSide() && handCard.getDarkCardType() == CardType.WILD) {
-            setDarkWildCardColor(handCard, topDiscardCard, chosenWildDarkCardColor);
-            replaceDeckCard(handCard);
+        // light side wild cards
+        else if (side && (handCard.getBrightColor() == CardColor.WILD)) {
+            System.out.println("In placeCard handCard LIGHT");
+            handCard.setBrightColor(selectedColor);
+            if (handCard.getBrightCardType() == CardType.WILD_DRAW) {
+                nextPlayer.addCardToHand(deck.drawCard());
+                nextPlayer.addCardToHand(deck.drawCard());
+            }
 
-         status = false;
-         } else if (!this.getSide() && handCard.getDarkCardType() == CardType.WILD_DRAW){
-            setDarkWildCardColor(handCard, topDiscardCard, chosenWildDarkCardColor);
-            replaceDeckCard(handCard);
+            displayedPlayer.removeCardFromHand(handCard);
+            deck.addToDiscardPile(handCard);
+            isPlaced = true;   //Returns status of placeCards - Milestone  3----------------
 
-         status = false;
-         }
-        else {
-            System.out.println("Invalid Card");
-            status = true;
         }
-        for (GameUpdate v : updateView) {
-            v.handleUnoUpdate(new GameEvent(this, topDiscardCard, handCard, status));
-        }
-    }
-    public void setChosenWildLightCardColor(String chosenWildLightCardColor) {
-        this.chosenWildLightCardColor = chosenWildLightCardColor;
-    }
+        // dark side wild cards
+        else if (!side && (handCard.getDarkColor() == CardColor.WILD)) {
+            System.out.println("In placeCard handCard DARK");
+            handCard.setDarkColor(selectedColor);
+            if (handCard.getDarkCardType() == CardType.WILD_DRAW) {
+                Card drawnCard = new Card(null,null,null,null);
+                while (drawnCard.getDarkColor() != selectedColor){
+                    drawnCard = deck.drawCard();
+                    nextPlayer.addCardToHand(drawnCard);
+                }
+            }
 
-    public void setChosenWildDarkCardColor(String chosenWildDarkCardColor) {
-        this.chosenWildDarkCardColor = chosenWildDarkCardColor;
+            displayedPlayer.removeCardFromHand(handCard);
+            deck.addToDiscardPile(handCard);
+            isPlaced = true;   //Returns status of placeCards - Milestone  3---------------
+        }
+        //updateStatus();
+        for (GameUpdate view : views) {
+            view.handlePlaceCardEvent(new PlaceCardEvent(this, displayedPlayer.getHand(), deck.topCardFromDiscardPile()));
+        }
+        return  isPlaced;
     }
 }
