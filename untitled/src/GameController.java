66da4a570565a03b9a64cf80b95c4171e99bc011
Nikita <147352295+nikitasv457@users.noreply.github.com>
2023-12-03 19:56:10 -0500

    Implemented card Image to button

--- untitled/src/GameController.java
+++ untitled/src/GameController.java
@@ -5,53 +5,47 @@ import java.awt.event.ActionListener;
 public class GameController implements ActionListener {
     Game model;
     GameView view;
+
+    /**
+     *  GameController constructor initializes the model and view parameters.
+     * @param model
+     * @param view
+     */
     public GameController(Game model, GameView view){
         this.model = model;
         this.view = view;
     }
+
+    /**
+     * Gets the user selected changes on the GUI for the specific action performed
+     * @param e
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         JButton source = (JButton) e.getSource();
         if (source.getText().equals("Next Player")){
-
-            CardColor selectedColor = null;
-            if (source.getText().contains("WILD")){
-                selectedColor = view.getWildCardColor();
-            }
-            model.nextPlayer(selectedColor);
-            view.centerPanel.getComponent(0).setEnabled(true);
-
-            // AI - Disables the cards panel - Milestone  3-----------------
+            model.nextPlayer();
+            view.enablePanel();
             if(model.getCurrentPlayer().getName().contains("AI")) {
-                int componentCnt = view.handPanel.getComponents().length;
-                for (int i = 0; i < componentCnt; i++) {
-                    view.handPanel.getComponent(i).setEnabled(false);
-                }
-                view.centerPanel.getComponent(0).setEnabled(false); //Disables draw button
+                view.disablePanel();
             }
-            // ------------------
         }
         else if (source.getText().equals("Draw Card")) {
             model.drawCard();
+            view.disablePanel();
         }
-
         else {
             CardColor selectedColor = null;
-            if (source.getText().contains("WILD")){
+            //if (source.getText().contains("WILD")){
+            if (source.getToolTipText().contains("WILD")){    // replaced getText() with getToolTipText() *************Milestone 4
                 selectedColor = view.getWildCardColor();
             }
             try {
                 String strNumber = source.getActionCommand();
                 int cardIndex = Integer.parseInt(strNumber);
-
-                //Disables the cards panel for Human player //Milestone  3--------------------
                 boolean isPlaced = model.placeCard(cardIndex, selectedColor);
                 if(isPlaced) {
-                    int componentCnt = view.handPanel.getComponents().length;
-                    for (int i = 0; i < componentCnt; i++) {
-                        view.handPanel.getComponent(i).setEnabled(false);
-                    }
-                    view.centerPanel.getComponent(0).setEnabled(false);
+                    view.disablePanel();
                 }
             } catch (NumberFormatException error) {
                 System.out.println("The string is not a valid integer.");
